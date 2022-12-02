49e49357b70819bf1c979f00d8c1ed02c4ae55cf
Dan Chalmers <danc@brandwatch.com>
2022-12-02 12:22:37 +0000

    day two works

---
+++ advent_of_code_2022/main.py
@@ -0,0 +1,7 @@
+from advent_of_code_2022.day_one import day_one
+from advent_of_code_2022.day_two import day_two_part_one, day_two_part_two
+
+if __name__ == "__main__":
+    day_one()
+    day_two_part_one()
+    day_two_part_two()
