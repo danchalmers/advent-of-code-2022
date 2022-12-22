1f403c0fd0f56392889b9e4cedec4b09fd2b7c4a
Dan Chalmers <danc@brandwatch.com>
2022-12-22 15:46:26 +0000

    day seven both parts working

--- advent_of_code_2022/main.py
+++ advent_of_code_2022/main.py
@@ -1,6 +1,7 @@
 from advent_of_code_2022.day_five import day_five
 from advent_of_code_2022.day_four import day_four
 from advent_of_code_2022.day_one import day_one
+from advent_of_code_2022.day_seven import day_seven
 from advent_of_code_2022.day_six import day_six
 from advent_of_code_2022.day_three import day_three
 from advent_of_code_2022.day_two import day_two_part_one, day_two_part_two
@@ -13,3 +14,4 @@ if __name__ == "__main__":
     day_four()
     day_five()
     day_six()
+    day_seven()
