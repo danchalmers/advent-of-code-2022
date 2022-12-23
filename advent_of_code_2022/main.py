548a23d761edaee55f1b15848cef12d41f406323
Dan Chalmers <danc@brandwatch.com>
2022-12-23 13:55:27 +0000

    day ten part one working and tidied a bit

--- advent_of_code_2022/main.py
+++ advent_of_code_2022/main.py
@@ -4,6 +4,7 @@ from advent_of_code_2022.day_four import day_four
 from advent_of_code_2022.day_one import day_one
 from advent_of_code_2022.day_seven import day_seven
 from advent_of_code_2022.day_six import day_six
+from advent_of_code_2022.day_ten import day_ten
 from advent_of_code_2022.day_three import day_three
 from advent_of_code_2022.day_two import day_two_part_one, day_two_part_two
 
@@ -17,3 +18,4 @@ if __name__ == "__main__":
     day_six()
     day_seven()
     day_eight()
+    day_ten()
